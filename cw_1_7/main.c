981271aedca8392b596b7f8800b1c456afd4ac00
LikeaGNU <entropia+@tutanota.com>
2025-08-02 20:48:13 +0200

    Dodanie ćwiczenia 1.7

---
+++ cw_1_7/main.c
@@ -0,0 +1,14 @@
+/*
+ * Język C. Programowanie (K&R)
+ * Ćwiczenie 1.7
+ * Napisz program, który wypisuje wartość EOF
+ */ 
+
+#include <stdio.h>
+
+int main() {
+	printf("%d\n", EOF);
+
+	return 0;
+}
+
